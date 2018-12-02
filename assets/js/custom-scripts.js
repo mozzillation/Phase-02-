0fe61598bc419a64462511f3daaa175941d7af44
Giuliano Mozzillo <mozzillation@me.com>
2018-12-02 15:52:14 +0100

    02 dic

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,44 +1,26 @@
-//// QUESTION 01 
-//
-//$('#seealsology').find('.category_nodes').on('mousemove', function (evt) {
-//    var x = evt.pageX - $(this).offset().left + 10;
-//    var y = evt.pageY - $(this).offset().top + -20;
-//
-//    $('.category_nodes__popup').css({
-//        top: y,
-//        left: x
-//    });
-//
-//    var parent = $(this).parent();
-//
-//
-//    parent.find('circle').hover(function () {
-//            var $this = $(this);
-//            var pageName = $this.data('name');
-//            parent.find('.category_nodes__popup').addClass('is-visible').html(pageName);
-//            parent.find('circle').not($this).addClass('hide');
-//        },
-//        function () {
-//            $('.category_nodes__popup').removeClass('is-visible')
-//            $('circle').removeClass('hide');
-//        });
-//
-//
-//    $('circle').click(function () {
-//        var pageName = $(this).data('name');
-//        pageName = pageName.replace(/ /g, '_');
-//        console.log(pageName);
-//        window.open('https://en.wikipedia.org/wiki/' + pageName);
-//
-//    })
-//
-//});
-//
-//
-//
-//$('circle').each(function (i) {
-//    var row = $(this);
-//    setTimeout(function () {
-//        row.toggleClass('is-visible', !row.hasClass('is-visible'));
-//    }, 10 * i);
-//});
+$(document).ready(function () {
+
+    if ($('#research').length) {
+
+        $.get("./assets/images/roadmap.svg", function (data) {
+            var svg = new XMLSerializer().serializeToString(data.documentElement);
+            $('#research').append(svg);
+
+
+
+            $('#research #links > g').click(function () {
+                var url = $(this).attr("id");
+                url = url.replace('_2_', '');
+
+                window.open('./' + url);
+
+            })
+
+
+        });
+
+
+
+    }
+
+});
